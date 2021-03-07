206c45ce69f77b2134d3b62e377a46419bf84e1e
MartaOstash <martaostash.18@gmail.com>
2021-03-07 11:02:28 -0500

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        // write your code here
+        for (int i = 0; i < 1; i++) {
+            System.out.println("Testing");
+            Thread.sleep(2000);
+        }
+    }}
